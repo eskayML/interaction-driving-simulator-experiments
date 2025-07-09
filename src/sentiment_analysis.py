925c5ab2080c5d664d8785f4b592fb00b89a6803
eskayML <kalusam619@gmail.com>
2025-07-09 22:01:05 +0100

    add(): implemented vader sentiment analysis

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -1,29 +1,21 @@
 # ===================================== ENFORCE CACHE DIRECTORY======================================
 import os
+
 os.environ["TORCH_HOME"] = "./model_weights/torch"
 os.environ["HF_HOME"] = "./model_weights/huggingface"
 os.environ["PYANNOTE_CACHE"] = "./model_weights/torch/pyannote"
 # ====================================================================================================
-from transformers import pipeline
-import torch
 import warnings
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 warnings.filterwarnings("ignore")
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+analyzer = SentimentIntensityAnalyzer()
+
 
 def analyze_sentiment(text):
-    """Perform sentiment analysis and return a composite score from -1 to +1."""
-    sentiment_pipeline = pipeline(
-        "sentiment-analysis",
-        model="cardiffnlp/twitter-roberta-base-sentiment",
-        tokenizer="cardiffnlp/twitter-roberta-base-sentiment",
-        return_all_scores=True,
-    )
-    sentiment_pipeline.to(device)
-    
-    result = sentiment_pipeline(text)[0]
-    scores = {item["label"]: item["score"] for item in result}
-    negative = scores.get("LABEL_0", 0)
-    neutral = scores.get("LABEL_1", 0)
-    positive = scores.get("LABEL_2", 0)
-    sentiment_score = (positive * 1) + (neutral * 0) + (negative * -1)
+    """Perform sentiment analysis using VADER and return a composite score from -1 to +1."""
+    scores = analyzer.polarity_scores(text)
+    sentiment_score = scores["compound"]
     return sentiment_score
